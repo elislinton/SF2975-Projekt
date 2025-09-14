7c3f8b65364f60e37c7e704e7312b6dccdade167
elislinton <elisli@kth.se>
2025-09-14 14:53:59 +0200

    Update Longstaff-Schwarz.py

--- Longstaff-Schwarz.py
+++ Longstaff-Schwarz.py
@@ -1,3 +1,4 @@
+# Task b)
 # Least-Squares Monte Carlo (Longstaff-Schwartz) for American put option pricing
 # The code will simulate paths under risk-neutral GBM, then use least-squares regression
 # on in-the-money paths to estimate continuation values and decide early exercise.
@@ -107,4 +108,5 @@ price, stderr = lsm_american_put_price(S0, K, T, r, sigma, n_sim=n_sim, n_steps=
 european_put = black_scholes_put(S0, K, T, r, sigma)
 
 print(price, stderr, european_put)
+
 
